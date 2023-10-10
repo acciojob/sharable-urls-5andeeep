9866c50bf4dcda8a1f16f2a31a9ae4a591774992
Sandeep <newsandeep03@gmail.com>
2023-10-10 15:01:47 +0530

    ans added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+function updateUrl(){
+    var name = document.getElementById('name').value;
+    var year = document.getElementById('year').value;
+    if(name && year){
+        document.getElementById('url').textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+    }
+    else if(name){
+        document.getElementById('url').textContent = `https://localhost:8080/?name=${name}`;
+    }
+    else if(year){
+        document.getElementById('url').textContent = `https://localhost:8080/?year=${year}`;
+    }
+}
